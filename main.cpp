af30106d3cebcaae5be4c2c7e3f993e54a9c88da
aaryaputraa <157559416+aaryaputraa@users.noreply.github.com>
2024-01-24 11:24:20 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, i, j;
+
+    cout << "Tinggi Segitiga N=?";
+    cin >> N;
+
+    for (i = 1; i <= N; i++) {
+        for (j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << "Program oleh Arya Eka Putra" << endl;
+
+    return 0;
+}
